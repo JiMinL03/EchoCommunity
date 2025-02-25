c4332dc12e84097744911ae0b57afd4f507efe28
jiminL03 <wlalswwkd0@naver.com>
2025-02-26 01:04:08 +0900

    DEPT 엔티티 NULL 오류 해결 #2

--- echoCommunity/src/main/java/com/project/EchoCommunity/Repository/DeptRepository.java
+++ echoCommunity/src/main/java/com/project/EchoCommunity/Repository/DeptRepository.java
@@ -3,5 +3,5 @@ package com.project.EchoCommunity.Repository;
 import com.project.EchoCommunity.Entity.Dept;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DeptRepository extends JpaRepository<Dept, Integer> {
+public interface DeptRepository extends JpaRepository<Dept, String> {
 }
